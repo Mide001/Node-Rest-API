8f0c0fb3e84257fb28c09601f8e58049b13ee8d0
Mide001 <olamideisreal5@gmail.com>
2023-11-24 00:01:23 +0100

    created router

---
+++ routes/BlogRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const { getAllBlogs, createBlog, getBlogById, updateBlog, deleteBlog } = require("../controllers/BlogController");
+
+router.route("/").get(getAllBlogs).post(createBlog);
+router.route("/:id").get(getBlogById).patch(updateBlog).delete(deleteBlog);
+
+module.exports = router;
