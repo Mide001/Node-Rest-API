e69a11b61dc431adb1342f049fd2e18579a02491
Mide001 <olamideisreal5@gmail.com>
2023-11-23 09:34:35 +0100

    added model for user and blog, configured server,js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next ) => {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            message: err.message,
+        },
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
